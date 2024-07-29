e466751042327c324d636a284b4db3849f02d7eb
kuangyuwu <kuangyuwu95@gmail.com>
2024-07-29 00:54:19 -0500

    activity_feed.js

---
+++ activity_feed.js
@@ -0,0 +1,49 @@
+function clearActivityFeed() {
+  const activityFeed = document.getElementById("activity-feed");
+  activityFeed.innerHTML = "";
+}
+
+function postActivity(innerHTMLNodes) {
+  const activityFeed = document.getElementById("activity-feed");
+
+  const activityOuter = document.createElement("div");
+  activityOuter.setAttribute("class", "activity-outer");
+  const activityInner = document.createElement("div");
+  activityInner.setAttribute("class", "activity-inner");
+  activityOuter.appendChild(activityInner);
+  for (const node of innerHTMLNodes) {
+    activityInner.appendChild(node);
+  }
+
+  activityFeed.append(activityOuter);
+}
+
+function activityRoll() {
+  const rollPrompt = document.createElement("span");
+  rollPrompt.innerHTML = "Roll the dices:&nbsp;";
+  const rollButton = document.createElement("button");
+  rollButton.innerHTML = "Roll";
+  rollButton.onclick = () => {
+    clearActivityFeed();
+    postActivity(activityDices([2, 3, 4, 5]));
+  };
+  return [rollPrompt, rollButton];
+}
+
+function activityDices(points) {
+  const text = document.createElement("span");
+  text.innerHTML = "Dices:&nbsp;";
+  const dices = [text];
+  for (const point of points) {
+    dices.push(dice(point));
+  }
+  return dices;
+}
+
+function dice(point) {
+  const dice = document.createElement("span");
+  dice.setAttribute("class", `dice-${point}`);
+  return dice;
+}
+
+export { clearActivityFeed, postActivity, activityRoll, activityDices };
--- script.js
+++ script.js
@@ -1,9 +1,18 @@
 import { initGameboard } from "./gameboard.js";
+import {
+  clearActivityFeed,
+  postActivity,
+  activityRoll,
+  activityDices,
+} from "./activity_feed.js";
 
 function main() {
   console.log("abc");
   const pathLengths = [-1, -1, 3, 5, 7, 9, 11, 13, 11, 9, 7, 5, 3];
   const gameboard = initGameboard(pathLengths);
+
+  postActivity(activityRoll());
+  postActivity(activityDices([1, 3, 5, 5]));
 }
 
 main();
